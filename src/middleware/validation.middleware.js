1ef1f1ea61537847f1a0a10a64315c292ae82344
bryanbill <emobrya@gmail.com>
2023-11-21 09:39:38 +0300

    ft: adds validation schema

---
+++ src/middleware/validation.middleware.js
@@ -0,0 +1,97 @@
+import { checkSchema } from "express-validator";
+
+const tokenSchema = checkSchema({
+    token: {
+        in: ["body"],
+        notEmpty: {
+            errorMessage: "Token is required",
+        },
+        trim: true,
+    },
+});
+
+const loginSchema = checkSchema({
+    email: {
+        in: ["body"],
+        isEmail: {
+            errorMessage: "Invalid email",
+        },
+        trim: true,
+    },
+    password: {
+        in: ["body"],
+        isLength: {
+            errorMessage: "Password should be at least 6 chars long",
+            options: { min: 6 },
+        },
+    },
+});
+
+const registerSchema = checkSchema({
+    email: {
+        in: ["body"],
+        isEmail: {
+            errorMessage: "Invalid email",
+        },
+        trim: true,
+    },
+    firstname: {
+        in: ["body"],
+        errorMessage: "Firstname is required",
+        trim: true,
+    },
+    lastname: {
+        in: ["body"],
+        errorMessage: "Lastname is required",
+        trim: true,
+    },
+    password: {
+        in: ["body"],
+        isLength: {
+            errorMessage: "Password should be at least 6 chars long",
+            options: { min: 6 },
+        },
+    },
+    confirmPassword: {
+        in: ["body"],
+        isLength: {
+            errorMessage: "Password should be at least 6 chars long",
+            options: { min: 6 },
+        },
+        custom: {
+            options: (value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error("Password confirmation does not match password");
+                }
+                return true;
+            },
+        },
+    },
+});
+
+const resetPasswordSchema = checkSchema({
+    password: {
+        in: ["body"],
+        isLength: {
+            errorMessage: "Password should be at least 6 chars long",
+            options: { min: 6 },
+        },
+    },
+    confirmPassword: {
+        in: ["body"],
+        isLength: {
+            errorMessage: "Password should be at least 6 chars long",
+            options: { min: 6 },
+        },
+        custom: {
+            options: (value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error("Password confirmation does not match password");
+                }
+                return true;
+            },
+        },
+    },
+});
+
+export { loginSchema, registerSchema, resetPasswordSchema, tokenSchema };
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/index.js";
+import { loginSchema, registerSchema, resetPasswordSchema, tokenSchema } from "../middleware/validation.middleware.js";
+import { body, query, validationResult } from "express-validator";
 
 const router = Router();
 const authController = new AuthController();
 
-router.post("/login", async (req, res) => {
+router.post("/login", loginSchema, async (req, res) => {
     try {
-        const { email, password } = req.body;
-        if (!email || !password) throw new Error("Invalid credentials");
+        const result = validationResult(req);
+        if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
 
         const user = await authController.login(req.body);
         if (!user) return res.status(401).send({ message: "Invalid credentials" });
@@ -16,27 +18,46 @@ router.post("/login", async (req, res) => {
         return res.status(500).send({ message: error.message });
     }
 });
-router.post("/register", async (req, res) => {
+
+router.post("/register", registerSchema, async (req, res) => {
     try {
+        const result = validationResult(req);
+        if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
+
         const user = await authController.register(req.body);
         return res.status(200).send(user);
     } catch (error) {
         return res.status(500).send({ message: error.message });
     }
 });
-router.post("/logout", (req, res) => {
+
+router.post("/logout", tokenSchema, (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
+
     res.send("Logout");
 });
-router.post("/refresh", (req, res) => {
+
+router.post("/refresh", tokenSchema, (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
+    
     res.send("Refresh");
 });
-router.get("/forgot-password", (req, res) => {
+router.get("/forgot-password", query("email", "Invalid email").notEmpty().trim().isEmail(), (req, res) => {
     res.send("Forgot Password");
 });
-router.post("/reset-password", (req, res) => {
+router.post("/reset-password", resetPasswordSchema, (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
+
     res.send("Reset Password");
 });
-router.get("/verify", (req, res) => {
+
+router.patch("/verify", tokenSchema, (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
+
     res.send("Verify email");
 });
 
