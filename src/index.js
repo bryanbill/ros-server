dbf8c4c5d224a6163667cbb91969bcc23b81faf0
bryanbill <emobrya@gmail.com>
2023-11-21 09:40:00 +0300

    improves error handling on 'init'

--- src/index.js
+++ src/index.js
@@ -3,22 +3,29 @@ import cors from "cors";
 import { config, database } from "./config/index.js";
 import morgan from "morgan";
 import router from "./routes/index.js";
+import chalk from "chalk";
 
 const init = async () => {
-    const db = await database.connect().then(async (db) => await db.sync());
-    if (!db) return;
+    var db;
+    try {
+        db = await database.connect().then(async (db) => await db.sync());
+        if (!db) return;
 
-    const app = express();
-    app.use(cors())
-        .use(express.json())
-        .use(express.urlencoded({ extended: true }))
+        const app = express();
+        app.use(cors())
+            .use(express.json())
+            .use(express.urlencoded({ extended: true }))
 
-    config.ENV === 'development' ? app.use(morgan('dev')) : null;
-    app.use(`/api/${config.VERSION}`, router);
+        config.ENV === 'development' ? app.use(morgan('dev')) : null;
+        app.use(`/api/${config.VERSION}`, router);
 
-    app.listen(config.PORT, () => {
-        console.log("Server running on PORT", config.PORT)
-    })
+        app.listen(config.PORT, () => {
+            console.log("Server running on PORT", config.PORT)
+        })
+    } catch (err) {
+        console.log(chalk.red("Error on init", err))
+        db.close();
+    }
 }
 
 init();
