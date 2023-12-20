bee5b6e0afc5bc7791d84dd9866fc7d64ebfba48
bryanbill <emobrya@gmail.com>
2023-12-20 09:29:08 +0300

    ft:Queue - implements queue service

--- src/services/queue.service.js
+++ src/services/queue.service.js
@@ -1,11 +1,34 @@
-import Queue from 'bull'
+import Bull from 'bull'
 import { config } from '../config/index.js'
 import { NotificationService } from './index.js'
 
 export class QueueService {
-    constructor() {
-        
+    static bull = new Bull('notification', {
+        url: config.Redis.host,
+        port: config.Redis.port
+    });
+    static async init() {
+        this.bull.process(async (job, done) => {
+            try {
+                const { type, data } = job.data;
+                switch (type) {
+                    case "email":
+                        await NotificationService.sendEmail(data);
+                        break;
+                    default:
+                        break;
+                }
+                done();
+            } catch (error) {
+                done(error);
+            }
+        });
     }
-
-
+    /**
+     * @param {string} type
+     * @param {object} data
+     */
+    static queue(type, data) {
+        this.bull.add({ type, data })
+    }
 }
