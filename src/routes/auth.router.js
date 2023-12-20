7ce487ff62048bd004c97c5e8afed662e9bc31c7
bryanbill <emobrya@gmail.com>
2023-12-20 09:32:04 +0300

    refactor: add param typings

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -2,25 +2,31 @@ import { Router } from "express";
 import { AuthController } from "../controllers/index.js";
 import { loginSchema, registerSchema, resetPasswordSchema, tokenSchema } from "../middleware/index.js";
 import { query, validationResult } from "express-validator";
+import { Request, Response } from "express";
 
 const router = Router();
 const authController = new AuthController();
 
 
-router.post("/login", loginSchema, async (req, res) => {
-    const result = validationResult(req);
-    if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
+router.post("/login", loginSchema, async (/**@type {Request} */req, /**@type {Response} */ res) => {
+    try {
+        const result = validationResult(req);
 
-    authController
-        .login(req.body).then((user) => {
-            if (!user) return res.status(401).send({ message: "Invalid credentials" });
-            return res.status(200).send(user);
-        }).catch((err) => {
-            return res.status(401).send({ message: err.message });
-        });
+        if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
+
+        authController
+            .login(req.body).then((user) => {
+                if (!user) return res.status(401).send({ message: "Invalid credentials" });
+                return res.status(200).send(user);
+            }).catch((err) => {
+                return res.status(401).send({ message: err.message });
+            });
+    } catch (error) {
+        return res.status(500).send({ message: error.message });
+    }
 });
 
-router.post("/register", registerSchema, async (req, res) => {
+router.post("/register", registerSchema, async (/**@type {Request} */req, /**@type {Response} */ res) => {
     try {
         const result = validationResult(req);
         if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
@@ -32,30 +38,37 @@ router.post("/register", registerSchema, async (req, res) => {
     }
 });
 
-router.post("/logout", tokenSchema, (req, res) => {
+router.post("/logout", tokenSchema, (/**@type {Request} */req, /**@type {Response} */ res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
 
     res.send("Logout");
 });
 
-router.post("/refresh", tokenSchema, (req, res) => {
+router.post("/refresh", tokenSchema, (/**@type {Request} */req, /**@type {Response} */ res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
 
     res.send("Refresh");
 });
-router.get("/forgot-password", query("email", "Invalid email").notEmpty().trim().isEmail(), (req, res) => {
-    res.send("Forgot Password");
-});
-router.post("/reset-password", resetPasswordSchema, (req, res) => {
+
+router.get("/forgot-password",
+    query("email", "Invalid email")
+        .notEmpty()
+        .trim()
+        .isEmail(),
+    (/**@type {Request} */req, /**@type {Response} */ res) => {
+        res.send("Forgot Password");
+    });
+
+router.post("/reset-password", resetPasswordSchema, (/**@type {Request} */req, /**@type {Response} */ res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
 
     res.send("Reset Password");
 });
 
-router.patch("/verify", tokenSchema, (req, res) => {
+router.patch("/verify", tokenSchema, (/**@type {Request} */req, /**@type {Response} */ res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) return res.status(400).send({ message: result.array()[0].msg });
 
